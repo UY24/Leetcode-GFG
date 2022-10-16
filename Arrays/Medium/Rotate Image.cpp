6a0da2c851286eb93191bf293e08947e7a4643f3
UY24 <ujjwal.uy03@gmail.com>
2022-10-16 15:18:41 +0530

    https://leetcode.com/problems/rotate-image/

---
+++ Arrays/Medium/Rotate Image.cpp
@@ -0,0 +1,13 @@
+// Ques Link : https://leetcode.com/problems/rotate-image/
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotate(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < i; j++)
+            swap(matrix[i][j], matrix[j][i]);
+    for (int i = 0; i < n; i++)
+        reverse(matrix[i].begin(), matrix[i].end());
+}
