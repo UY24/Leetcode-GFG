9886e1f3ae0082deec07c41dfd9fb6b32f52ddd0
UY24 <ujjwal.uy03@gmail.com>
2023-02-20 19:21:33 +0530

    2 approach TC O(N)

---
+++ Binary Search/Medium/33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,44 @@
+// Ques Link: https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Approach 1 TC O(N)
+
+int search(vector<int> &nums, int target)
+{
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] == target)
+            return i;
+    }
+    return -1;
+}
+
+// Approach 2 TC O(Log N)
+
+int search(vector<int> &nums, int target)
+{
+    int s = 0, n = nums.size(), e = n - 1;
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+        if (nums[mid] == target)
+            return mid;
+        else if (nums[s] <= nums[mid])
+        { //  s to md is sorted
+            if (nums[mid] > target && nums[s] <= target)
+                e = mid - 1;
+            else
+                s = mid + 1;
+        }
+        else if (nums[e] >= nums[mid])
+        { //  s to md is sorted
+            if (nums[mid] < target && nums[e] >= target)
+                s = mid + 1;
+            else
+                e = mid - 1;
+        }
+    }
+    return -1;
+}
