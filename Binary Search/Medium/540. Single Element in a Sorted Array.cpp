effd87aecf30a49484ad2ffd39b298a3943d68dc
UY24 <ujjwal.uy03@gmail.com>
2023-02-21 11:30:12 +0530

    TC O(N)

---
+++ Binary Search/Medium/540. Single Element in a Sorted Array.cpp
@@ -0,0 +1,23 @@
+// Ques link : https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int singleNonDuplicate(vector<int> &nums)
+{
+    int s = 0, n = nums.size(), e = n - 1;
+    while (s < e)
+    {
+        int mid = s + (e - s) / 2;
+        if (nums.size() > 2)
+        {
+            if ((mid % 2 == 0 && nums[mid + 1] == nums[mid]) || (mid % 2 != 0 && nums[mid - 1] == nums[mid]))
+            {
+                s = mid + 1;
+            }
+            else
+                e = mid;
+        }
+    }
+    return nums[s];
+}
